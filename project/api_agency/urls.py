a243de0210a61fc693b355c25af41288791f5b18
Yacine DAOUD <77809130+D-Y2000@users.noreply.github.com>
2024-02-24 20:12:08 +0000

    Update urls.py

--- project/api_agency/urls.py
+++ project/api_agency/urls.py
@@ -3,25 +3,27 @@ from api_agency.views import *
 from rest_framework.authtoken.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 urlpatterns = [
-    path('login/',ObtainAuthToken.as_view(),),
-    path('logout/',logOut),
+    #JWT VIEWS
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    #Agency
     path('agencies/',Agencies.as_view()),
     path('agencies/<int:pk>/',AgencyDetails.as_view()),
     path('agency/profile/',agencyProfile),
     path('agencies/<int:pk>/branches/',AgencyBranches.as_view()),
     path('agency/branches/<int:pk>/',AgencyBranchesDetails.as_view()),
     path('agencies/<int:pk>/vehicles/',AgencyVehicles.as_view()),
+    #Branches
     path('branches/',Branches.as_view()),
     path('branches/<int:pk>/',BranchDetails.as_view()),
-
+    #search and filter view + creation
     path('makes/',vehicles_makes),
     path('makes/<int:pk>/models/',vehicles_models),
-    #search and filter view + creation
+
+    
     path('vehicles/',ListVehicles.as_view()),
     path('vehicles/<int:pk>/',VehicleDetails.as_view()),
 
-    #JWT VIEWS
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
 ]
