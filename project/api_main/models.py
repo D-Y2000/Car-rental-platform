96eee2732158fff64874a32781f06ed01dc95dac
daishek <daichekkal@gmail.com>
2024-03-23 23:30:40 +0100

    update reservation + like with user model to avoid circular import issues

--- project/api_main/models.py
+++ project/api_main/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from api_agency.models import User,Agency
 import datetime
+from django.contrib.auth import get_user_model
 
-
-
-# Create your models here.
+User = get_user_model()
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     date_of_birth = models.DateField()
     profile_image = models.ImageField(null=True,blank=True)
 
     def get_age(self):
-        now =datetime.date.now()
-        return now - self.date_of_birth 
+        now = datetime.date.today()
+        age = now.year - self.date_of_birth.year - ((now.month, now.day) < (self.date_of_birth.month, self.date_of_birth.day))
+        return age
+
 
     def __str__(self) -> str:
         return f'{self.first_name}  {self.last_name}'
