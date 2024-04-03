2c022b8501fd4401e6d15f65a9c1c536b78df04b
daishek <daichekkal@gmail.com>
2024-04-04 00:35:03 +0100

    add frontend domain to alowed cors origins

--- project/project/settings.py
+++ project/project/settings.py
@@ -196,4 +196,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000',]
+    'http://127.0.0.1:3000',
+    'https://caroussa.vercel.app/'
+    ]
