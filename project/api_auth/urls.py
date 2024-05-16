7b4fc7627071917b3999bbad1c16f754304ac01b
Yacine DAOUD <77809130+D-Y2000@users.noreply.github.com>
2024-05-16 14:22:36 +0100

    Added login view for agency and client

--- project/api_auth/urls.py
+++ project/api_auth/urls.py
@@ -3,7 +3,9 @@ from api_auth.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 urlpatterns = [
         #JWT VIEWS
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('client/login/', ClientLogin.as_view()),
+    path('agency/login/', AgencyLogin.as_view()),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('user/me/',UserProfile.as_view()),
