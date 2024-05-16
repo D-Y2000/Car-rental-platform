6b4dfaf4a83fc99ad6e4519e74a2cbd6789dd2f6
Yacine DAOUD <77809130+D-Y2000@users.noreply.github.com>
2024-05-16 18:27:28 +0100

    Update urls.py

--- project/api_auth/urls.py
+++ project/api_auth/urls.py
@@ -3,7 +3,7 @@ from api_auth.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 urlpatterns = [
         #JWT VIEWS
-    path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('client/login/', ClientLogin.as_view()),
     path('agency/login/', AgencyLogin.as_view()),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
