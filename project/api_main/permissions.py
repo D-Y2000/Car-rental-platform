40124d63a54a15aa81467c4f7c9b8707b4464d15
Yacine DAOUD <77809130+D-Y2000@users.noreply.github.com>
2024-07-09 14:38:41 +0100

    Fixed rates,feedbacks and reports permissions

--- project/api_main/permissions.py
+++ project/api_main/permissions.py
@@ -45,6 +45,23 @@ class CandDeleteReservation(permissions.BasePermission):
             return True
         return obj.status == 'postponed'
     
+class CanRateAndFeedback(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        user = request.user
+        profile = Profile.objects.get(user = user)
+        agency_pk = view.kwargs.get('pk')
+        try:
+            agency = Agency.objects.get(pk=agency_pk)
+        except Agency.DoesNotExist:
+            return False
+        reservations = Reservation.objects.filter(client = profile, agency = agency)
+        if reservations:
+            return True
+        else:
+            return False
+
 class CanEditFeedback(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
